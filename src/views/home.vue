1e040c5759bc6f8fecc06a99f5c5ee85e743bf95
demi5555 <senkropum10@gmail.com>
2026-02-02 22:50:27 +0700

    fix responsive layout

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,63 +1,86 @@
 <template>
-    <section>
-        <div class="container text-light py-5">
-            <div class="text w-100 text-end fs-6">
-                <h3>
-                    DECEMBER / 2025 <br>
-                    BRIGITTE SCHWARTZ
-                </h3>
-            </div>
-            <div class="text text-uppercase w-100 text-start mt-3">
-                <h2>
-                    <span class="firstName">
-                        nhoem
-                    </span>
-                    <span class="lastName">
-                        senkropum
-                    </span>
-                </h2>
-            </div>
-            <div class="text d-flex justify-content-between">
-                <div class="desc text-start">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium et fugit quas voluptatibus exercitationem asperiores autem. Dolorem mollitia eligendi sint dicta ullam iste, quisquam quos deleniti. Ea eos magni quaerat?</div>
-                <div class="desc text-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ullam nostrum quisquam cum quis sed pariatur iste. Aperiam nisi pariatur id nulla consequatur nostrum, repellendus odio impedit, ea qui possimus?</div>
-            </div>
+  <section>
+    <div class="container text-light py-5">
+
+      <div class="text w-100 text-end fs-6">
+        <h3>
+          DECEMBER / 2025 <br>
+          BRIGITTE SCHWARTZ
+        </h3>
+      </div>
+
+      <div class="text text-uppercase w-100 text-start mt-3">
+        <h2 class="mb-0">
+          <span class="firstName d-block">
+            nhoem
+          </span>
+          <span class="lastName d-block text-center">
+            senkropum
+          </span>
+        </h2>
+      </div>
+
+      <!-- ✔ correct Bootstrap responsive layout -->
+      <div class="text row g-3 mt-3">
+
+        <div class="col-12 col-lg-6">
+          <div class="desc text-start">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium et fugit quas voluptatibus exercitationem asperiores autem.
+          </div>
         </div>
-    </section>
-    <section>
-        <div class="container d-flex gap-2">
-            <div v-for="btn in myBtns" class="allBtns">
-                <BaseBtn :title="btn"/>
-            </div>
+
+        <div class="col-12 col-lg-6 text-lg-end">
+          <div class="desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ullam nostrum quisquam cum quis sed pariatur iste.
+          </div>
         </div>
-    </section>
+
+      </div>
+
+    </div>
+  </section>
+
+  <section class="py-5 ">
+    <h1 class="text-light" style="font-size: 5rem;">Table Content</h1>
+    <div class="container d-flex flex-wrap gap-2">
+      <div
+        v-for="btn in myBtns"
+        :key="btn"
+        class="allBtns"
+      >
+        <BaseBtn :title="btn"/>
+      </div>
+    </div>
+  </section>
 </template>
+
 <style scoped>
+
 .firstName{
-    line-height: 1;
-    margin: 0;
-    font-size: 6rem;
+  line-height: 1;
+  margin: 0;
+  font-size: clamp(2rem, 6vw, 6rem);
 }
+
 .lastName{
-    line-height: 1;
-    margin: 0;
-    font-size: 22rem;
+  line-height: 1;
+  margin: 0;
+  font-size: clamp(4rem, 18vw, 22rem);
 }
+
 .desc{
-    font-size: 1.5rem;
-    width: 35%;
+  font-size: 1.5rem;
 }
 </style>
 
 <script setup>
-import BaseBtn from '@/component/buttons/BaseBtn.vue';
-import { reactive } from 'vue';
+import BaseBtn from '@/component/buttons/BaseBtn.vue'
+import { reactive } from 'vue'
 
-
-
-let myBtns =  reactive([
-    'Introduction',
-    'Experience',
-    'Skills',
-    'Education'
+const myBtns = reactive([
+  'Introduction',
+  'Experience',
+  'Skills',
+  'Education'
 ])
 </script>
